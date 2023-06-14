6479595ff7ef3bbc47c023694db07d5cf363d210
Thiago Padua <paduathiago@outlook.com>
2023-06-14 00:11:52 -0300

    adding function to determine the value of s

--- src/tp2virtual.c
+++ src/tp2virtual.c
@@ -12,12 +12,26 @@ FILE * openFile(char *file) {
     return file;
 }
 
+int sValue(int pageSize)
+{
+    int tmp = pageSize;
+    int s = 0;
+    while (tmp > 1) 
+    {
+        tmp = tmp >> 1;
+        s++;
+    }
+    return s;
+}
+
 int main(int argc, char *argv[])
 {
     char * algorithm = argv[1];
     char * fileName = argv[2];
-    int page_size = atoi(argv[3]);
-    int memorySize = atoi(argv[4]);
+    unsigned pageSize = atoi(argv[3]);
+    unsigned memorySize = atoi(argv[4]);
+
+    unsigned s = sValue(pageSize);
 
     if (argc != 5)
     {
@@ -32,16 +46,20 @@ int main(int argc, char *argv[])
     else if (algorithm == 'fifo')
     {
         FILE * file = openFile(fileName);
-        
-        char address[9];
+        unsigned addr, page;
         char mode;
 
-        while (fscanf(file, "%8s %c", address, &mode) == 2) 
+        // The queue size is the memory size divided by the page size. This represents the number of pages in memory
+        struct Queue * queue = createQueue(memorySize/pageSize);  
+        while (fscanf(file, "%8s %c", &addr, &mode) == 2) 
         {
-            
+            page = addr >> s;
+            // Chamar método para enfileirar a página.
+            // Verificar se a página já está na memória.
+            // Se não estiver, verificar se a memória está cheia.
+            // Se estiver, desenfileirar a página mais antiga.
+            // Enfileirar a página atual.
         }
-        
-        struct Queue * queue = createQueue(memorySize);
         fclose(file);
     }
     else if (algorithm == 'lru')
